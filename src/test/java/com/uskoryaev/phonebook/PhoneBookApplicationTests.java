22bbb60b6769f362df8c345fd129a4eeda2e34d7
ProgrammStart <ProgrammStart625@gmail.com>
2020-10-06 20:53:10 +0300

    Initial commit

---
+++ src/test/java/com/uskoryaev/phonebook/PhoneBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uskoryaev.phonebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhoneBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
